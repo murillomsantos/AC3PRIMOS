e45137700fd73546659d5922c191e8147d518c7c
murillomsantos <126617621+murillomsantos@users.noreply.github.com>
2023-04-17 20:05:20 -0300

    Update and rename README.md to ac3_primos.py

---
+++ ac3_primos.py
@@ -0,0 +1,26 @@
+# AC3PRIMOS
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/primos')
+def primos():
+    primos = []
+    num = 2
+    while len(primos) < 100:
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primos.append(num)
+        num += 1
+    primos_str = ""
+    for i in primos:
+        primos_str += str(i) + " "
+    return primos_str
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
